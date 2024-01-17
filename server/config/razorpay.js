0b5fc347facdd0a8cdd6285886568797c15e68d7
CWCArvind <115211980+CWCArvind@users.noreply.github.com>
2024-01-17 23:13:25 +0530

    .env urls updated with backticks

--- server/config/razorpay.js
+++ server/config/razorpay.js
@@ -2,6 +2,6 @@ const Razorpay = require("razorpay");
 require("dotenv").config();
 
 exports.instance = new Razorpay({
-	key_id: process.env.RAZORPAY_KEY,
-	key_secret: process.env.RAZORPAY_SECRET,
+	key_id: `${process.env.RAZORPAY_KEY}`,
+	key_secret: `${process.env.RAZORPAY_SECRET}`,
 });
